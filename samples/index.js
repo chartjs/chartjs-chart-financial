1a68a448b8ed004e6d3eadea9482f7ff6e9d4ea8
Ben McCann <benjamin.j.mccann@gmail.com>
2017-04-26 10:42:37 -0700

    More realistic fake sample data

--- samples/index.js
+++ samples/index.js
@@ -2,23 +2,27 @@ function randomNumber(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-function randomBar(date) {
+function randomBar(date, lastClose) {
+	var open = randomNumber(lastClose * .95, lastClose * 1.05);
+	var close = randomNumber(open * .95, open * 1.05);
+	var high = randomNumber(Math.max(open, close), Math.max(open, close) * 1.1);
+	var low = randomNumber(Math.min(open, close) * .9, Math.min(open, close));
 	return {
 		t: date.valueOf(),
-		o: randomNumber(30, 40),
-		h: randomNumber(40, 50),
-		l: randomNumber(20, 30),
-		c: randomNumber(30, 40)
+		o: open,
+		h: high,
+		l: low,
+		c: close
 	};
 }
 
 var dateFormat = 'MMMM DD YYYY';
 var date = moment('April 01 2017', dateFormat);
-var data = [randomBar(date)];
+var data = [randomBar(date, 30)];
 while (data.length < 60) {
 	date = date.add(1, 'd');
 	if (date.isoWeekday() <= 5) {
-		data.push(randomBar(date));
+		data.push(randomBar(date, data[data.length - 1].c));
 	}
 }
 
