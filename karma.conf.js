7557eb36475e3b6d0234292ad5101b24dd2cb9fd
Ben McCann <ben@benmccann.com>
2017-05-25 09:18:18 -0700

    Upgrade Chart.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,30 @@
-module.exports = function(config) {
-	var configuration = {
+module.exports = function(karma) {
+	var config = {
 		browsers: ['Firefox'],
-		files: ['./node_modules/chart.js/dist/Chart.js', './node_modules/chart.js/test/mockContext.js', './Chart.Financial.js', './test/**/*.js'],
-		frameworks: ['jasmine'],
+		files: ['./node_modules/chart.js/dist/Chart.js', './Chart.Financial.js', './test/**/*.js'],
+		frameworks: ['browserify', 'jasmine'],
 		reporters: ['progress', 'kjhtml'],
+		preprocessors: {
+			'./test/jasmine.index.js': ['browserify'],
+			'./src/**/*.js': ['browserify']
+		},
+		browserify: {
+			debug: true
+		}
 	};
 
 	// If on the CI, use the CI chrome launcher
 	if (process.env.TRAVIS) {
-		configuration.browsers.push('Chrome_travis_ci');
-		configuration.customLaunchers = {
+		config.browsers.push('Chrome_travis_ci');
+		config.customLaunchers = {
 			Chrome_travis_ci: {
 				base: 'Chrome',
 				flags: ['--no-sandbox']
 			}
 		};
 	} else {
-		configuration.browsers.push('Chrome');
+		config.browsers.push('Chrome');
 	}
 
-	config.set(configuration);
+	karma.set(config);
 };
