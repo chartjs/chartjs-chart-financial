349d7d78e4b0469721efa81bb953f6f4c2acaa86
Ben McCann <ben@benmccann.com>
2018-01-28 12:47:56 -0800

    Remove old test. Timeseries scale now lives upstream

--- test/specs/scale.timeseries.tests.js
+++
@@ -1,9 +0,0 @@
-describe('Timeseries Scale', function() {
-
-	it('Should register the constructor with the scale service', function() {
-		var Constructor = Chart.scaleService.getScaleConstructor('timeseries');
-		expect(Constructor).not.toBe(undefined);
-		expect(typeof Constructor).toBe('function');
-	});
-
-});
