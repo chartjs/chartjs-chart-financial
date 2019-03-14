2ade589f8027dab1b4d6a4250a415fe19d88266a
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-03-14 10:11:35 -0700

    Improve sample Chart

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,11 @@
-var candleCount = 60;
+var barCount = 60;
 var initialDateStr = '01 Apr 2017 00:00 Z';
-var data = getRandomData(initialDateStr, candleCount);
+var data = getRandomData(initialDateStr, barCount);
 
-// Candlestick
-var ctx = document.getElementById("chart1").getContext("2d");
+var ctx = document.getElementById("chart").getContext("2d");
 ctx.canvas.width = 1000;
 ctx.canvas.height = 250;
-var candlestickChart = new Chart(ctx, {
+var chart = new Chart(ctx, {
 	type: 'candlestick',
 	data: {
 		datasets: [{
@@ -16,32 +15,47 @@ var candlestickChart = new Chart(ctx, {
 	}
 });
 
-// OHLC
-var ctx2 = document.getElementById("chart2").getContext("2d");
-ctx2.canvas.width = 1000;
-ctx2.canvas.height = 250;
-var ohlcChart = new Chart(ctx2, {
-	type: 'ohlc',
-	data: {
-		datasets: [{
-			label: "CHRTO - Chart.js Corporation, OHLC division",
-			data: data
-		}]
-	}
-});
-
 var getRandomInt = function(max) {
 	return Math.floor(Math.random() * Math.floor(max));
-}
+};
+
+var update = function() {
+	var dataset = chart.config.data.datasets[0];
+
+	// candlestick vs ohlc
+	var type = document.getElementById('type').value;
+	dataset.type = type;
+
+	// color
+	var colorScheme = document.getElementById('color-scheme').value;
+	if (colorScheme === 'neon') {
+		dataset.color = {
+			up: '#01ff01',
+			down: '#fe0000',
+			unchanged: '#999',
+		};
+	} else {
+		delete dataset.color;
+	}
+
+	// border
+	var border = document.getElementById('border').value;
+	if (border === 'true') {
+		dataset.borderColor = '#000';
+		dataset.borderWidth = 2;
+	} else {
+		delete dataset.borderColor;
+		delete dataset.borderWidth;
+	}
+
+	chart.update();
+};
+
+document.getElementById('update').addEventListener('click', update);
 
 document.getElementById('randomizeData').addEventListener('click', function() {
-	candlestickChart.data.datasets.forEach(function(dataset) {
-		dataset.data = getRandomData(initialDateStr, candleCount + getRandomInt(10));
+	chart.data.datasets.forEach(function(dataset) {
+		dataset.data = getRandomData(initialDateStr, barCount + getRandomInt(10));
 	});
-	candlestickChart.update();
-
-	ohlcChart.data.datasets.forEach(function(dataset) {
-		dataset.data = getRandomData(initialDateStr, candleCount + getRandomInt(10));
-	});
-	ohlcChart.update();
+	update();
 });
