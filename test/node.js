c1f1ed31bdb6282587677f798629b818f1bd84fd
SeanSobey <sean.m.sobey@gmail.com>
2021-11-28 01:41:27 +0200

    Fixed lint errors

--- test/node.js
+++ test/node.js
@@ -1,10 +1,8 @@
-const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
-const luxon = require('luxon');
+const {ChartJSNodeCanvas} = require('chartjs-node-canvas');
 const path = require('path');
 const assert = require('assert').strict;
 
 async function main() {
-
 	const chartJSNodeCanvas = new ChartJSNodeCanvas({
 		width: 400, height: 400, plugins: {
 			globalVariableLegacy: [
@@ -20,25 +18,25 @@ async function main() {
 				label: 'CHRT - Chart.js Corporation',
 				data: [
 					{
-						"x": 1491004800000,
-						"o": 29.18,
-						"h": 31.62,
-						"l": 28.99,
-						"c": 29.97
+						x: 1491004800000,
+						o: 29.18,
+						h: 31.62,
+						l: 28.99,
+						c: 29.97
 					},
 					{
-						"x": 1491177600000,
-						"o": 30.8,
-						"h": 31.27,
-						"l": 29.11,
-						"c": 30.52
+						x: 1491177600000,
+						o: 30.8,
+						h: 31.27,
+						l: 29.11,
+						c: 30.52
 					},
 					{
-						"x": 1491264000000,
-						"o": 30.16,
-						"h": 33.34,
-						"l": 28.84,
-						"c": 30.44
+						x: 1491264000000,
+						o: 30.16,
+						h: 33.34,
+						l: 28.84,
+						c: 30.44
 					}
 				]
 			}]
